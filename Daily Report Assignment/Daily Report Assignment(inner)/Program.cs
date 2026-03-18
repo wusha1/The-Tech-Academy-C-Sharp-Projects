c158bdd4735c503641b5d1800b005b14ef839c2e
wusha1 <allcompass11@outlook.com>
2026-03-18 03:23:23 -0700

    Fix nested repo + add Daily Report Assignment

---
+++ Daily Report Assignment/Daily Report Assignment(inner)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("The Tech Academy.");
+        Console.WriteLine("Student Daily Report.");
+
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("What course are you on?");
+        string course = Console.ReadLine();
+
+        Console.WriteLine("What page number?");
+        int pageNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+        bool needsHelp = Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+        string positiveExperience = Console.ReadLine();
+
+        Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+        string feedback = Console.ReadLine();
+
+        Console.WriteLine("How many hours did you study today?");
+        decimal hoursStudied = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+    }
+}
