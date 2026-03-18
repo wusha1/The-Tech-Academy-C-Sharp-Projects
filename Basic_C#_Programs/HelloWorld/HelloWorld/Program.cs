ee12b28fc26862b992a564f1f66f59a1e6e08603
wusha1 <allcompass11@outlook.com>
2026-03-18 01:18:13 -0700

    Initial commit - C# Course Projects

---
+++ Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is simple programm
+            //Console.WriteLine("What is you name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("what is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte current Temperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyinBank = 100.5m;
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Jesse";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathOperationsAssignment/MathOperationsAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("10 + 5 = " + (10 + 5));
+        Console.WriteLine("10 - 5 = " + (10 - 5));
+        Console.WriteLine("10 * 5 = " + (10 * 5));
+        Console.WriteLine("10 / 5 = " + (10 / 5));
+    }
+}
