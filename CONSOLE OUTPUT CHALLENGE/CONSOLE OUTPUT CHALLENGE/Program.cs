55bd722267d00a442226c062574de165afb30291
wusha1 <allcompass11@outlook.com>
2026-03-22 18:57:21 -0700

    CONSOLE OUTPUT CHALLENGE

---
+++ CONSOLE OUTPUT CHALLENGE/CONSOLE OUTPUT CHALLENGE/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Greater than >
+        if (number > 10)
+        {
+            Console.WriteLine("Your number is greater than 10.");
+        }
+
+        // Less than or equal to <=
+        if (number <= 10)
+        {
+            Console.WriteLine("Your number is less than or equal to 10.");
+        }
+
+        Console.ReadLine();
+    }
+}
