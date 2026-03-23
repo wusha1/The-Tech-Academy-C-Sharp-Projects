350eef0505248e15f49eb5a2c92a2a4e145bb3fd
wusha1 <allcompass11@outlook.com>
2026-03-22 18:51:38 -0700

    Add Operator != Console

---
+++ OPERATOR CHALLENGE/OPERATOR CHALLENGE/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your favorite number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number != 10)
+        {
+            Console.WriteLine("Your number is not 10");
+        }
+        else
+        {
+            Console.WriteLine("Your number is 10");
+        }
+
+        Console.ReadLine();
+    }
+}
