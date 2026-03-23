ad3048bd4ef99811732c1b38875ca91f33b83da0
wusha1 <allcompass11@outlook.com>
2026-03-22 18:14:15 -0700

    Boolean logic asisgnment

---
+++ Boolean Logic/Boolean Logic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your age?");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Have you ever had a DUI? (true/false)");
+        bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine("How many speeding tickets do you have?");
+        int tickets = Convert.ToInt32(Console.ReadLine());
+
+        // Qualification rules
+        bool qualified = (age > 15 && !hasDUI && tickets <= 3);
+
+        Console.WriteLine("Qualified?");
+        Console.WriteLine(qualified);
+
+        Console.ReadLine();
+    }
+}
