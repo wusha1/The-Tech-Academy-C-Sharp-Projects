b981eb19e2b73b9e9caafddf3b8a9ad8924f0754
wusha1 <allcompass11@outlook.com>
2026-03-22 19:29:11 -0700

    TERNARY OPERATOR CHALLENGE

---
+++ TERNARY OPERATOR CHALLENGE/TERNARY OPERATOR CHALLENGE/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        string result = (age >= 18) ? "You are an adult." : "You are a minor.";
+
+        Console.WriteLine(result);
+
+        Console.ReadLine();
+    }
+}
