d48ce73b67c037162418d8e57cf14144f33c1eb0
wusha1 <allcompass11@outlook.com>
2026-03-22 18:27:26 -0700

    Add Conditional Statements Challenge

---
+++ CONDITIONAL STATEMENTS/CONDITIONAL STATEMENTS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 10)
+        {
+            Console.WriteLine("The number is greater than 10.");
+        }
+        else if (number == 10)
+        {
+            Console.WriteLine("The number is exactly 10.");
+        }
+        else
+        {
+            Console.WriteLine("The number is less than 10.");
+        }
+
+        Console.ReadLine();
+    }
+}
