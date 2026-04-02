f5847ad5171f70e41029fa97fb47ed5e8186bbf9
wusha1 <allcompass11@outlook.com>
2026-04-01 19:14:44 -0700

    ITERATION PART ONE VIDEO

---
+++ ITERATION PART ONE VIDEO/ITERATION PART ONE VIDEO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+        //for (int i = 0; i < testScores.Length; i++)
+        //{
+        //    if (testScores[i] > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + testScores[i]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+        string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+        for (int j = 0; j < names.Length; j++)
+        {
+            if (names[j] == "Jesse")
+        {
+                Console.WriteLine(names[j]);
+    }
+}
