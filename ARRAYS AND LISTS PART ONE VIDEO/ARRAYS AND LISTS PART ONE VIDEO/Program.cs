e04dbf2a92627c39b460a6fe4b0a29f6ed2501a0
wusha1 <allcompass11@outlook.com>
2026-04-01 18:06:16 -0700

    ARRAYS AND LISTS PART ONE VIDEO

---
+++ ARRAYS AND LISTS PART ONE VIDEO/ARRAYS AND LISTS PART ONE VIDEO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numArray = new int[5];
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] = 200;
+        numArray[4] = 5000;
+
+        int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+        Console.WriteLine(numArray1[3]);
+        Console.ReadLine();
+    }
+}
