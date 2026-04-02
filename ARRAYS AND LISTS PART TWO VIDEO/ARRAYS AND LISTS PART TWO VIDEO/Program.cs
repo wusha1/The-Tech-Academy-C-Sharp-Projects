56607413d0d894f31088c1a5b9093c9aebcb9189
wusha1 <allcompass11@outlook.com>
2026-04-01 18:31:28 -0700

    Array video 2

---
+++ ARRAYS AND LISTS PART TWO VIDEO/ARRAYS AND LISTS PART TWO VIDEO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> intList = new List<string>();
+
+        intList.Add("Hello");
+        intList.Add("Jesse");
+        intList.Remove("Jesse");
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+
+        byte[] byteArray = new byte[5000];
+    }
+}
