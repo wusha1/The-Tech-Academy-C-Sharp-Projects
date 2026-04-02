1361f4144c8e09cbb2005d18e2e0d3aab460c371
wusha1 <allcompass11@outlook.com>
2026-04-01 21:26:54 -0700

    Created one-dimensional string array

---
+++ CONSOLE APP ASSIGNMENT/CONSOLE APP ASSIGNMENT/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. One-dimensional array of strings
+        string[] words = { "Apple", "Banana", "Cherry", "Orange" };
+
+        // 2. Ask the user for input
+        Console.WriteLine("Enter some text to add to each word:");
+        string userInput = Console.ReadLine();
+
+        // 3. Loop through array and add user input
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = words[i] + " " + userInput;
+        }
+
+        // 4. Loop to print each string on a new line
+        Console.WriteLine("\nUpdated words:");
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+
+        // Keeps console open (important for testing)
+        Console.ReadLine();
+    }
+}
--- ITERATION PART ONE VIDEO/ITERATION PART ONE VIDEO/Program.cs
+++ ITERATION PART ONE VIDEO/ITERATION PART ONE VIDEO/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
 class Program
 {
     static void Main(string[] args)
     {
-        //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+        //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };A
 
         //for (int i = 0; i < testScores.Length; i++)
         //{
@@ -17,12 +16,52 @@ class Program
         //}
         //Console.ReadLine();
 
-        string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+        //string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
 
-        for (int j = 0; j < names.Length; j++)
-        {
-            if (names[j] == "Jesse")
-        {
-                Console.WriteLine(names[j]);
-    }
-}
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //    if (names[j] == "Jesse")
+        //    {
+        //        Console.WriteLine(names[j]);
+        //    }
+        //}
+
+        //Console.ReadLine();
+
+        //List<int> testScores = new List<int>();
+        //testScores.Add(98);
+        //testScores.Add(99);
+        //testScores.Add(81);
+        //testScores.Add(72);
+        //testScores.Add(70);
+
+        //foreach (int score in testScores)
+        //{ 
+        //    if (score > 85)
+        //    {
+        //        Console.WriteLine("Passing test score:" + score);
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //List<string> names = new List<string>(); { "Jesse", "Erik", "Adam", "Daniel" };
+
+        //foreach (string name in names)
+        //{
+        //        Console.WriteLine(name);
+        //}
+        //Console.ReadLine();
+
+        List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+        List<int> passingScores = new List<int>();
+
+        foreach (int acore in testScores)
+        { 
+            if (score > 85)
+            {
+                passingScores.Add(acore);
+            }
+        }
+
+        Console.WriteLine(passingScores.Count);
+        Console.ReadLine();
