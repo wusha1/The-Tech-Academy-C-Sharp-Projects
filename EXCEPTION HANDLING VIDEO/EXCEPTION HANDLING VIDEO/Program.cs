bc29550484b0be5fe5afa5dc5bc38ac050820376
wusha1 <allcompass11@outlook.com>
2026-04-02 20:13:53 -0700

    EXCEPTIONAL HANDLING VIDEO ASSIGNMENT.

---
+++ EXCEPTION HANDLING VIDEO/EXCEPTION HANDLING VIDEO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Pick a nummber.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + "divide by" + numberTwo + " equals " + numberThree);
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        { 
+            Console.WriteLine("Please type a whole number.");
+            return;
+    }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+}
