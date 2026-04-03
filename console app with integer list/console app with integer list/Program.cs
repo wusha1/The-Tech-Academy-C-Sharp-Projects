9ff049bb661be3014069c246353e9e3d6412b480
wusha1 <allcompass11@outlook.com>
2026-04-02 20:23:56 -0700

    Created console app with integer list division and try/catch exception handling

---
+++ console app with integer list/console app with integer list/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>() { 10, 20, 30, 40, 50 };
+
+        Console.WriteLine("Enter a number to divide each number in the list by:");
+
+        try
+        {
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number / userNumber);
+            }
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        Console.WriteLine("The program has emerged from the try/catch block and continued execution.");
+        Console.ReadLine();
+    }
+}
