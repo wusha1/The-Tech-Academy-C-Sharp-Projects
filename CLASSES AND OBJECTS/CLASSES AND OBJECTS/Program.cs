1c583d7e2f58062fd3a9eba3e3b4e83028468abb
wusha1 <allcompass11@outlook.com>
2026-04-07 19:21:43 -0700

    Class and Objects

---
+++ CLASSES AND OBJECTS/CLASSES AND OBJECTS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+    }
+}
