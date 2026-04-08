40f809cfbaac8784c28623de83f529592268e16a
wusha1 <allcompass11@outlook.com>
2026-04-07 20:05:36 -0700

    Class and Objectives assignment.

---
+++ CLASSES AND OBJECTS/CLASSES AND OBJECTS/Card.cs
@@ -0,0 +1,8 @@
+﻿namespace TwentyOne
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
--- CLASSES AND OBJECTS/CLASSES AND OBJECTS/Program.cs
+++ CLASSES AND OBJECTS/CLASSES AND OBJECTS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TwentyOne
 {
@@ -7,13 +8,31 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
+            deck = Shuffle(deck);
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
+
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> tempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                tempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+
+            deck.Cards = tempList;
+            return deck;
         }
     }
 }
