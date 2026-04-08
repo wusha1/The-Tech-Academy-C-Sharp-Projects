6fa033ec4f3ea46a124f71e61a99b5bb8606dc46
wusha1 <allcompass11@outlook.com>
2026-04-07 20:12:24 -0700

    Added math methods and called them in Main

---
+++ MathOperations.cs/MathOperations.cs/MathOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TwentyOne
+{
+    public class MathOperations
+    {
+        public int AddTen(int number)
+        {
+            return number + 10;
+        }
+
+        public int MultiplyByTwo(int number)
+        {
+            return number * 2;
+        }
+
+        public int SubtractFive(int number)
+        {
+            return number - 5;
+        }
+    }
+}
---
+++ MathOperations.cs/MathOperations.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            Console.WriteLine("Enter a number:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            int result1 = math.AddTen(userNumber);
+            int result2 = math.MultiplyByTwo(userNumber);
+            int result3 = math.SubtractFive(userNumber);
+
+            Console.WriteLine("Add 10: " + result1);
+            Console.WriteLine("Multiply by 2: " + result2);
+            Console.WriteLine("Subtract 5: " + result3);
+
+            Console.ReadLine();
+        }
+    }
+}
