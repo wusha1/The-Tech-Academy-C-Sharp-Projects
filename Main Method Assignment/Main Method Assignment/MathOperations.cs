6e2d8677fac186879840e0b4ce5b21566864eb8c
wusha1 <allcompass11@outlook.com>
2026-04-09 20:10:35 -0700

    Added overloaded methods assignment

---
+++ Main Method Assignment/Main Method Assignment/MathOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    public class MathOperations
+    {
+        public int DoMath(int number)
+        {
+            return number + 10;
+        }
+
+        public int DoMath(decimal number)
+        {
+            return (int)(number * 2);
+        }
+
+        public int DoMath(string number)
+        {
+            int convertedNumber = Convert.ToInt32(number);
+            return convertedNumber - 5;
+        }
+    }
+}
---
+++ Main Method Assignment/Main Method Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            int result1 = math.DoMath(10);
+            Console.WriteLine("Integer result: " + result1);
+
+            int result2 = math.DoMath(12.5m);
+            Console.WriteLine("Decimal result: " + result2);
+
+            int result3 = math.DoMath("20");
+            Console.WriteLine("String result: " + result3);
+
+            Console.ReadLine();
+        }
+    }
+}
