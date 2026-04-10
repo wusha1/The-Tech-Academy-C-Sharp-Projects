8f1996a25347b721402bee148a2fe2c244c4f08e
wusha1 <allcompass11@outlook.com>
2026-04-09 20:16:42 -0700

    Added optional parameter method assignment

---
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/MathOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    public class MathOperations
+    {
+        public int DoMath(int num1, int num2 = 0)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            Console.WriteLine("Enter first number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number (or press Enter to skip):");
+            string secondInput = Console.ReadLine();
+
+            int result;
+
+            if (secondInput == "")
+            {
+                result = math.DoMath(firstNumber);
+            }
+            else
+            {
+                int secondNumber = Convert.ToInt32(secondInput);
+                result = math.DoMath(firstNumber, secondNumber);
+            }
+
+            Console.WriteLine("Result: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
