bac2b1c902fd89366dbf4228f50f36c6cc8e1f36
wusha1 <allcompass11@outlook.com>
2026-04-13 23:50:22 -0700

    Added methods and objects assignment

---
+++ METHODS AND OBJECTS SUBMISSION ASSIGNMENT/METHODS AND OBJECTS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Sample";
+            emp.LastName = "Student";
+
+            emp.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
