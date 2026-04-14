bfc4038e838774b735855a5a34eb06b131d520f0
wusha1 <allcompass11@outlook.com>
2026-04-13 19:12:01 -0700

    Added deck, card classes, and shuffle method for card game

--- CLASSES AND OBJECTS/CLASSES AND OBJECTS/Deck.cs
+++ CLASSES AND OBJECTS/CLASSES AND OBJECTS/Deck.cs
@@ -1,33 +1,59 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TwentyOne
 {
     public class Deck
     {
+        public List<Card> Cards { get; set; }
+
         public Deck()
         {
             Cards = new List<Card>();
 
-            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
+            List<string> suits = new List<string>()
+            {
+                "Clubs", "Hearts", "Diamonds", "Spades"
+            };
 
-            List<string> Faces = new List<string>()
+            List<string> faces = new List<string>()
             {
                 "Two", "Three", "Four", "Five", "Six", "Seven",
                 "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
             };
 
-            foreach (string face in Faces)
+            foreach (string face in faces)
             {
-                foreach (string suit in Suits)
+                foreach (string suit in suits)
                 {
-                    Card card = new Card();
-                    card.Suit = suit;
-                    card.Face = face;
+                    Card card = new Card
+                    {
+                        Suit = suit,
+                        Face = face
+                    };
+
                     Cards.Add(card);
                 }
             }
         }
 
-        public List<Card> Cards { get; set; }
+        public void Shuffle(int times = 1)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> tempList = new List<Card>();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    tempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+
+                Cards = tempList;
+            }
+        }
     }
 }
--- CLASSES AND OBJECTS/CLASSES AND OBJECTS/Program.cs
+++ CLASSES AND OBJECTS/CLASSES AND OBJECTS/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace TwentyOne
 {
@@ -8,8 +7,8 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            int timesShuffled = 0;
-            deck = Shuffle(deck, out timesShuffled, 3);
+
+            deck.Shuffle(3);
 
             foreach (Card card in deck.Cards)
             {
@@ -17,27 +16,17 @@ namespace TwentyOne
             }
 
             Console.WriteLine(deck.Cards.Count);
-            Console.WriteLine("Times shuffled: {0}", timesShuffled);
             Console.ReadLine();
         }
 
-        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
-        {
-            timesShuffled = 0;
-            Random random = new Random();
-
-            for (int i = 0; i < times; i++)
-            {
-                List<Card> tempList = new List<Card>();
-
-                while (deck.Cards.Count > 0)
-                {
-                    int randomIndex = random.Next(0, deck.Cards.Count);
-                    tempList.Add(deck.Cards[randomIndex]);
-                    deck.Cards.RemoveAt(randomIndex);
-            }
-            deck.Cards = TempList;
-            return deck;
-        }
+        // Old shuffle method (kept for reference)
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
     }
 }
