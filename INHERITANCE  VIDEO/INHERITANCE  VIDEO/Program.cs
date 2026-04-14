ed7d70dc2e02adde50f9e8f3170e84d91b409f7d
wusha1 <allcompass11@outlook.com>
2026-04-13 23:43:36 -0700

    Added inheritance example with Game and TwentyOneGame classes

---
+++ INHERITANCE  VIDEO/INHERITANCE  VIDEO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
+            game.ListPlayers();
+
+            Console.ReadLine();
+
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
+        }
+    }
+}
