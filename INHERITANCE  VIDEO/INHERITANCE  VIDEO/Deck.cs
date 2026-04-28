0b19e11711d0948ae78936436b11e4b90f3b0939
wusha1 <allcompass11@outlook.com>
2026-04-28 00:29:43 -0700

    Debugging Video

--- INHERITANCE  VIDEO/INHERITANCE  VIDEO/Deck.cs
+++ INHERITANCE  VIDEO/INHERITANCE  VIDEO/Deck.cs
@@ -37,6 +37,16 @@ namespace TwentyOne
         {
             for (int i = 0; i < times; i++)
             {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
             }
         }
     }
