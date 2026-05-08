3db41bdba83f122119d3250bef18c550b61bd72c
wusha1 <allcompass11@outlook.com>
2026-05-08 01:16:52 -0700

    Added generic Employee class with lists

---
+++ PARAMETERS SUBMISSION ASSIGNMENT/PARAMETERS SUBMISSION ASSIGNMENT/Employee.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ParametersAssignment
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+    }
+}
---
+++ PARAMETERS SUBMISSION ASSIGNMENT/PARAMETERS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Employee with strings
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Things = new List<string>()
+            {
+                "Shoes",
+                "Hat",
+                "Phone"
+            };
+
+            // Employee with integers
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int>()
+            {
+                1,
+                2,
+                3
+            };
+
+            // Print string list
+            foreach (string item in employee1.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Print integer list
+            foreach (int item in employee2.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
